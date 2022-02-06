f58a23ce75f67708cf94dcf2d1a2bd15a2875ab6
Abdiwahab <as1270@student.le.ac.uk>
2022-02-06 15:53:00 +0000

    Create homepage controller and manage default requests

---
+++ src/main/java/com/example/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Manages all requests to the homepage
+ * @author abdiwahab
+ * @version 1
+ */
+@Controller
+public class IndexController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+	
+
+}
