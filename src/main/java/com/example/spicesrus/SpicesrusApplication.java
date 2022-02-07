f86f7b9434263fe1351f5eca980ed8b3fc48c029
Callie Lewis <cl480@student.le.ac.uk>
2022-02-07 12:53:21 +0000

    Spices now appear on page from database

--- src/main/java/com/example/spicesrus/SpicesrusApplication.java
+++ src/main/java/com/example/spicesrus/SpicesrusApplication.java
@@ -24,5 +24,15 @@ public class SpicesrusApplication implements ApplicationRunner {
     	s1.setName("ALLSPICE");
     	s1.setPrice(0.8);
     	s1.setDescription("Allspice is not actually blend of other spices but a berry of an evergreen tree. It's flavor is similar to a blend of nutmeg, cloves and cinnamon");
+    	s1.setPicture("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/AllspiceBowl.JPG/220px-AllspiceBowl.JPG");
+    	s1 = spicesRepo.save(s1);
+    	
+    	Spices s2 = new Spices();
+    	s2.setName("BLACK PEPPER");
+    	s2.setPrice(0.99);
+    	s2.setDescription("Black Pepper adds flavor to almost every food of every nation in the world. It is used in rubs, spice blends, salad dressings, and peppercorn blends.");
+    	s2.setPicture("https://tse2.mm.bing.net/th?id=OIP.kAnWSBo2ETYfDSENYY1s8QHaE7&pid=Api");
+    	s2 = spicesRepo.save(s2);
+    	
     }
 }
--- src/main/java/com/example/spicesrus/controller/SpicesController.java
+++ src/main/java/com/example/spicesrus/controller/SpicesController.java
@@ -1,18 +1,20 @@
 package com.example.spicesrus.controller;
 
-import java.util.List;
+import com.example.spicesrus.repo.SpicesRepository;
 
-import com.example.spicesrus.entities.Spices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SpicesController {
+	@Autowired
+	private SpicesRepository spicesRepo;
 	
 	@RequestMapping("/spices")
 	public String Spices(Model model) {
-		List<Spices> spiceListFromDatabase = null; //Add this from Database
+		Iterable<com.example.spicesrus.entities.Spices> spiceListFromDatabase = spicesRepo.findAll(); 
 		model.addAttribute("spices", spiceListFromDatabase );
 		return "allSpices";
 	}
