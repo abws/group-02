f02632c6ab61d5c72eca91ca3af2104dd83cb769
halee <halee@LAPTOP-AD13DJV0>
2022-02-07 17:40:25 +0000

    Created classes and repositories for orders

--- src/main/java/com/example/spicesrus/entities/Orders.java
+++ src/main/java/com/example/spicesrus/entities/Orders.java
@@ -6,7 +6,6 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 @Entity
 public class Orders {
