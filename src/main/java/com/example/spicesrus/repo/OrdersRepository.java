9fc5c82e5220327c4bdb491406235cadee9ff3f4
halee <halee@LAPTOP-AD13DJV0>
2022-02-07 17:32:28 +0000

    Created Classes and repositories for orders

---
+++ src/main/java/com/example/spicesrus/repo/OrdersRepository.java
@@ -0,0 +1,10 @@
+package com.example.spicesrus.repo;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.spicesrus.entities.Orders;
+import com.example.spicesrus.entities.Spices;
+
+public interface OrdersRepository extends CrudRepository<Orders, Integer> {
+
+    public Spices findByName(String name);
+}
---
+++ src/main/java/com/example/spicesrus/security/BillingDetails.java
@@ -0,0 +1,57 @@
+package com.example.spicesrus.security;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class BillingDetails {
+@Id
+private int billingID;
+@OneToOne
+private int ordernum;
+private String firstName;
+private String surname;
+private String customerAddress;
+private String customerEmail;
+
+public int getBillingID() {
+	return billingID;
+}
+public void setBillingID(int billingID) {
+	this.billingID = billingID;
+}
+public String getFirstName() {
+	return firstName;
+}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+public String getSurname() {
+	return surname;
+}
+public void setSurname(String surname) {
+	this.surname = surname;
+}
+public String getCustomerAddress() {
+	return customerAddress;
+}
+public void setCustomerAddress(String customerAddress) {
+	this.customerAddress = customerAddress;
+}
+public String getCustomerEmail() {
+	return customerEmail;
+}
+public void setCustomerEmail(String customerEmail) {
+	this.customerEmail = customerEmail;
+}
+public int getOrdernum() {
+	return ordernum;
+}
+public void setOrdernum(int ordernum) {
+	this.ordernum = ordernum;
+}
+
+
+
+}
