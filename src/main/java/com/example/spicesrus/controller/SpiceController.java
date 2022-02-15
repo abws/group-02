bf063ab3c8007be781a4a1e53946df1fd6dbdec3
Abdiwahab <as1270@student.le.ac.uk>
2022-02-15 19:15:39 +0000

    Complete controller for individual spices and create jsp

--- src/main/java/com/example/spicesrus/controller/SpiceController.java
+++ src/main/java/com/example/spicesrus/controller/SpiceController.java
@@ -1,10 +1,13 @@
 package com.example.spicesrus.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.spicesrus.entities.Spices;
+import com.example.spicesrus.repo.SpicesRepository;
 
 /**
  * Manages all requests to the homepage
@@ -13,9 +16,14 @@ import com.example.spicesrus.entities.Spices;
  */
 @Controller
 public class SpiceController {
+	@Autowired
+	SpicesRepository sRepo;
+	
 	@RequestMapping("/spices") //by default manages get requests
-	public String showSpice(@RequestParam Spices spice) {
-		return null;
+	public String showSpice(@RequestParam Spices spice, Model model) {
+		Spices s = sRepo.findByName(spice.getName()); //same as finding by id since the name is the id
+		model.addAttribute("spice", s);
+		return "spice";
 		
 	}
 	
