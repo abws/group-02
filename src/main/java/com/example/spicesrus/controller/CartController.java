ac01497687f5c59757e66757cdcfafaa597262a6
Abdiwahab <as1270@student.le.ac.uk>
2022-03-04 11:47:23 +0000

    Premature session functionality test

--- src/main/java/com/example/spicesrus/controller/CartController.java
+++ src/main/java/com/example/spicesrus/controller/CartController.java
@@ -1,14 +1,33 @@
 package com.example.spicesrus.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.spicesrus.entities.Recipes;
+import com.example.spicesrus.repo.RecipesRepository;
+
 @Controller
 public class CartController {
+	@Autowired
+	RecipesRepository rRepo;
 	
 	@GetMapping("/cart")
-	public String getCart() {
-		return "cart";
+	public String getCart(HttpServletRequest request) {
+		Recipes r1 = new Recipes();
+    	r1.setCategory("Pastries");
+    	r1.setDescription("");
+    	r1.setName("Cinnamon Buns");
+    	r1.setPicture("https://www.cookingclassy.com/wp-content/uploads/2020/09/mini-cinnamon-rolls-21.jpg");
+    	rRepo.save(r1);
+		request.getSession().setAttribute("cart", r1.getName());
+		String x = (String) request.getSession().getAttribute("cart");
+		Recipes y = rRepo.findByName(x);
+		System.out.println(y);
+		
+		return "/";
 	}
 	
 	@PutMapping("/cart")
