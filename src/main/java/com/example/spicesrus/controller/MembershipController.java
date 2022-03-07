e3bf0f6be9d9197fac6bc2f2a0b02645ab553683
Connor Johncock <cj202@student.le.ac.uk>
2022-03-07 16:21:10 +0000

    Fix duplicate login button

--- src/main/java/com/example/spicesrus/controller/MembershipController.java
+++ src/main/java/com/example/spicesrus/controller/MembershipController.java
@@ -4,15 +4,11 @@ import com.example.spicesrus.security.UDetails;
 import com.example.spicesrus.security.UDetailsRepo;
 import com.example.spicesrus.security.UDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.security.Principal;
-import java.util.List;
 
 @Controller
 public class MembershipController {
