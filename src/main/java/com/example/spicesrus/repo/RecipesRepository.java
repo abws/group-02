68bcabd350393512f0b095c606739577dec43943
Jan Kaliszuk <jano@host-112-121.eduroam-local.wifi.le.ac.uk>
2022-03-09 12:43:10 +0000

    changes in recipesrepository

--- src/main/java/com/example/spicesrus/repo/RecipesRepository.java
+++ src/main/java/com/example/spicesrus/repo/RecipesRepository.java
@@ -1,10 +1,15 @@
 package com.example.spicesrus.repo;
 
 import com.example.spicesrus.entities.Recipes;
+import com.example.spicesrus.entities.Spices;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface RecipesRepository extends CrudRepository<Recipes, String> {
 
     public Recipes findByName(String name);
+    public List<Recipes> findByNameContainingIgnoreCase(String name);
 }
 
