b00ab59908a3a754592d6b91eead09254ea85259
Jan Kaliszuk <jano@MacBook-Air-Jan.local>
2022-03-13 19:59:51 +0000

    Modified the RecipesController

--- src/main/java/com/example/spicesrus/controller/RecipesController.java
+++ src/main/java/com/example/spicesrus/controller/RecipesController.java
@@ -1,18 +1,29 @@
 package com.example.spicesrus.controller;
 
+import com.example.spicesrus.SpicesrusApplication;
+import com.example.spicesrus.entities.Recipes;
 import com.example.spicesrus.repo.RecipesRepository;
+import com.example.spicesrus.security.UDetailsRepo;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class RecipesController {
     @Autowired
     private RecipesRepository recipesRepo;
+    
+    @Autowired
+	private UDetailsRepo uRepo;
 
     @RequestMapping("/recipes")
     public String Recipes(Model model) {
@@ -21,6 +32,139 @@ public class RecipesController {
         return "allRecipes";
     }
 
+	@GetMapping("/recipes/Breakfast")
+	public String showCategory_B(Model model) {
+		
+		List<Recipes> recipes = SpicesrusApplication.recipes;
+		List<Recipes> output = new ArrayList<Recipes>();
+		for (Recipes r: recipes) {
+			if (r.getCategory()=="Breakfast") {
+				output.add(r);
+			}
+		}
+		model.addAttribute("path", "Relevant");
+		model.addAttribute("recipes", output);
+				
+		return "allRecipes";
+	}
+	
+	@GetMapping("/recipes/Lunch")
+	public String showCategory_L(Model model) {
+		
+		List<Recipes> recipes = SpicesrusApplication.recipes;
+		List<Recipes> output = new ArrayList<Recipes>();
+		for (Recipes r: recipes) {
+			if (r.getCategory()=="Lunch") {
+				output.add(r);
+			}
+		}
+		model.addAttribute("path", "Relevant");
+		model.addAttribute("recipes", output);
+				
+		return "allRecipes";
+	}
+	
+	@GetMapping("/recipes/Dinner")
+	public String showCategory_D(Model model) {
+		
+		List<Recipes> recipes = SpicesrusApplication.recipes;
+		List<Recipes> output = new ArrayList<Recipes>();
+		for (Recipes r: recipes) {
+			if (r.getCategory()=="Dinner") {
+				output.add(r);
+			}
+		}
+		model.addAttribute("path", "Relevant");
+		model.addAttribute("recipes", output);
+				
+		return "allRecipes";
+	}
+	
+	@GetMapping("/recipes/Cakes and Pies")
+	public String showCategory_CaP(Model model) {
+		
+		List<Recipes> recipes = SpicesrusApplication.recipes;
+		List<Recipes> output = new ArrayList<Recipes>();
+		for (Recipes r: recipes) {
+			if (r.getCategory()=="Cakes and Pies") {
+				output.add(r);
+			}
+		}
+		model.addAttribute("path", "Relevant");
+		model.addAttribute("recipes", output);
+				
+		return "allRecipes";
+	}
+	
+	@GetMapping("/recipes/Pastires")
+	public String showCategory_P(Model model) {
+		
+		List<Recipes> recipes = SpicesrusApplication.recipes;
+		List<Recipes> output = new ArrayList<Recipes>();
+		for (Recipes r: recipes) {
+			if (r.getCategory()=="Pastires") {
+				output.add(r);
+			}
+		}
+		model.addAttribute("path", "Relevant");
+		model.addAttribute("recipes", output);
+				
+		return "allRecipes";
+	}
+	
+	@GetMapping("/recipes/a-z")
+	public String sort_a(Model model) {
+		
+		List<Recipes> recipes = SpicesrusApplication.recipes;
+		List<String> names = new ArrayList<String>();
+		List<Recipes> output = new ArrayList<Recipes>();
+		for (Recipes r: recipes) {
+			names.add(r.getName());			
+		}
+		Collections.sort(names);
+		for (String n: names) {
+			for (Recipes r: recipes) {
+				if (r.getName()== n) {
+					output.add(r);
+				}
+			}
+		}
+		model.addAttribute("recipes", output);	
+		model.addAttribute("path", "A-Z");
+		return "allRecipes";
+	}
+	
+	@GetMapping("/recipes/z-a")
+	public String sort_z(Model model) {
+		
+		List<Recipes> recipes = SpicesrusApplication.recipes;
+		List<String> names = new ArrayList<String>();
+		List<Recipes> output = new ArrayList<Recipes>();
+		for (Recipes r: recipes) {
+			names.add(r.getName());			
+		}
+		Collections.sort(names,Collections.reverseOrder());
+		for (String n: names) {
+			for (Recipes r: recipes) {
+				if (r.getName()== n) {
+					output.add(r);
+				}
+			}
+		}
+		
+		model.addAttribute("recipes", output);	
+		model.addAttribute("path", "Z-A");
+		return "allRecipes";
+	}
+	
+	
+	@GetMapping("/recipesearch")
+	public String search(Model model, @RequestParam String recipe) {
+		List<Recipes> recipes = recipesRepo.findByNameContainingIgnoreCase(recipe);
+		model.addAttribute("recipes", recipes);	
+		return "allRecipes";
+		
+	}
+	
 }
-	
-	
+
