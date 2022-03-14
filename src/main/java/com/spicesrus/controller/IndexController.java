2516ccd1cc6ed4b6001ed62e5d5b6d0fdfaa6ecd
Abdiwahab <as1270@student.le.ac.uk>
2022-03-14 16:51:03 +0000

    Delete dependency causing bug

--- src/main/java/com/spicesrus/controller/IndexController.java
+++ src/main/java/com/spicesrus/controller/IndexController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.example.spicesrus.entities.Cart;
 
 import java.security.Principal;
 
@@ -20,8 +19,6 @@ import javax.servlet.http.HttpServletRequest;
 public class IndexController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Principal principal, Model model, HttpServletRequest request) {
-		request.getSession().setAttribute("cart", new Cart());
-		request.getSession().setAttribute("cart", new Cart());
 		String username = principal != null ? principal.getName() : null;
 		model.addAttribute("username", username);
 		return "index/index";
