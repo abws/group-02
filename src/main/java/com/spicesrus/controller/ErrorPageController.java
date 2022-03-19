2f95c09322e1f2f548be5b685490e83be29faba4
Connor Johncock <cj202@student.le.ac.uk>
2022-03-19 06:15:15 +0000

    Implemented a basic error handling system (Need to change page design)

---
+++ src/main/java/com/spicesrus/controller/ErrorPageController.java
@@ -0,0 +1,29 @@
+package com.spicesrus.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ErrorPageController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        if (request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE) != null) {
+            int x = Integer.parseInt(String.valueOf(request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE)));
+            if (x  == HttpStatus.NOT_FOUND.value()) {
+                model.addAttribute("errorType", "404");
+            }else{
+                model.addAttribute("errorType", "");
+            }
+        }else{
+            model.addAttribute("errorType", "");
+        }
+        return "error";
+    }
+}
