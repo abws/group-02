042cec53ebe0fac059625bb60e2f09cafef8fd84
Connor Johncock <cj202@student.le.ac.uk>
2022-03-30 09:34:05 +0100

    Fix merge issue

--- src/main/java/com/spicesrus/controller/ItemController.java
+++ src/main/java/com/spicesrus/controller/ItemController.java
@@ -5,7 +5,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+
 import javax.servlet.http.HttpServletRequest;
+
+import com.spicesrus.model.*;
+import com.spicesrus.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,31 +17,20 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import com.spicesrus.model.Cart;
-import com.spicesrus.model.Item;
-import com.spicesrus.model.ItemImperial;
-import com.spicesrus.model.ItemMetric;
-import com.spicesrus.model.Spices;
-import com.spicesrus.model.User;
-import com.spicesrus.repository.CartRepository;
-import com.spicesrus.repository.ItemRepository;
-import com.spicesrus.repository.SpicesRepository;
-import com.spicesrus.repository.UserRepository;
-import com.spicesrus.service.CartHelper;
 
 /**
- * Manages the creation and destruction of item objects, and their addition to a cart
- * @author Abdiwahab
- * version 3
+ * Manages the creation and destruction of item objects, and adding them to a cart
+ *
+ * @author version 3
  */
 
 @Controller
 public class ItemController {
 
     @Autowired //for creating a new items in pounds
-    ItemPoundsRepository ipRepo;
+    ItemImperialRepository ipRepo;
     @Autowired //for creating new items in grams
-    ItemGramsRepository igRepo;
+    ItemMetricRepository igRepo;
     @Autowired //for displaying the spice
     SpicesRepository sRepo;
     @Autowired
@@ -62,8 +55,8 @@ public class ItemController {
         //get spice to view and item ready
         Spices s = sRepo.findByName(spice); //same as finding by id since the name is the id
         model.addAttribute("spice", s);
-        model.addAttribute("itemPound", new ItemPounds()); //CANNOT INSTANTIATE AN ITEM OBJECT!, whatever happened to that whole polymorphism stuff they were bragging about (would be nice if i could create a general object that gets specified after form is filled)
-        model.addAttribute("itemGram", new ItemGrams());
+        model.addAttribute("itemPound", new ItemImperial()); //CANNOT INSTANTIATE AN ITEM OBJECT!, whatever happened to that whole polymorphism stuff they were bragging about (would be nice if i could create a general object that gets specified after form is filled)
+        model.addAttribute("itemGram", new ItemMetric());
 
         //user privileges
         String level;
@@ -119,7 +112,7 @@ public class ItemController {
      * @return "spice" jsp page
      */
     @PostMapping("addItemGrams")
-    public String addItemGrams(@ModelAttribute ItemGrams item, HttpServletRequest request) {
+    public String addItemGrams(@ModelAttribute ItemMetric item, HttpServletRequest request) {
 
         Cart cart = (Cart) request.getSession().getAttribute("cart");
         if (cRepo.findById(cart.getId()).isPresent())
@@ -149,7 +142,7 @@ public class ItemController {
      * @return "spice" jsp page
      */
     @PostMapping("addItemPounds")
-    public String addItemPounds(@ModelAttribute ItemPounds item, HttpServletRequest request) {
+    public String addItemPounds(@ModelAttribute ItemImperial item, HttpServletRequest request) {
         Cart cart = (Cart) request.getSession().getAttribute("cart");
         cart.getItems().add(item);
         request.getSession().setAttribute("cart", cart);
@@ -162,103 +155,16 @@ public class ItemController {
         //return "redirect:/spice?spice=" + item.getSpice().getName();
         return "redirect:/spices";
     }
-	
+
 	/*
 	@PostMapping("deleteItem")
 	public String deleteItem(@RequestParam int id) {
 		Item i = iRepo.findById(id);
 		iRepo.deleteById(i.getId());
 
-		return "redirect:/cart";	
-	
-	/**
-	 * Individual spice page
-	 * Checks if a spice exists in the database.
-	 * Couldn't handle "/spices..." requests as its implemented elsewhere
-	 * @param spice
-	 * @param model
-	 * @return "spice" jsp page
-	 */
-	@RequestMapping("/spice") //by default manages get requests
-	public String showSpice(@RequestParam String spice, Model model, HttpServletRequest request, Principal user) {
-		String username = user != null ? user.getName() : null;
-		model.addAttribute("username", username);
-		//get spice to view and item ready 
-		Spices s = sRepo.findByName(spice); //same as finding by id since the name is the id
-		model.addAttribute("spice", s);
-		model.addAttribute("itemImperial", new ItemImperial());
-		model.addAttribute("itemMetric", new ItemMetric());
-		
-		
-		//user privileges
-		User userDetails = null;
-		String level;
-		
-		if (user == null)
-			level = "none";
-		else {
-			userDetails = uRepo.findByUsername(user.getName());
-			level = userDetails.getAuthorities().get(userDetails.getAuthorities().size() - 1);
-		}
-		
-		model.addAttribute("level", level.toLowerCase());
-	
-		
-		//in the case user types the spice name into the url
-		if (s != null)
-			model.addAttribute("spice", s);
-		else
-			return "spice-not-found-page";
-		return "spice";	
-	}	
-	
-	
-	/**
-	 * Adds an item in metric format to database
-	 * Item form takes us here
-	 * @param item
-	 * @return "spice" jsp page
-	 */
-	@PostMapping("addItemMetric")
-	public String addItemMetric(@ModelAttribute ItemMetric item, HttpServletRequest request) {
-		System.out.println(item.getWeight());
-			
-		Cart cart = CartHelper.createOrRetrieveCart(request);
-		item.setCart(cart);
+		return "redirect:/cart";
 
-		cart.getItems().add(item);
-		request.getSession().setAttribute("cart", cart);
-		
-		item.setCart(cart);
-		Item i = iRepo.save(item);
-		i = iRepo.findById(i.getId());
-		
-		CartHelper.setCartSize(cart.getItems().size());
-		//return "redirect:/spice?spice=" + item.getSpice().getName(); //change to shop after testing
-		return "redirect:/spices";
 	}
-	
-	/**
-	 * Adds an item in imperial format to database
-	 * Item form takes us here
-	 * @param item
-	 * @return "spice" jsp page
-	 */
-	@PostMapping("addItemImperial")
-		public String addItemPounds(@ModelAttribute ItemImperial item, HttpServletRequest request) {
-			
-		Cart cart = CartHelper.createOrRetrieveCart(request);
-		item.setCart(cart);
-	
-		cart.getItems().add(item);
-		request.getSession().setAttribute("cart", cart);
-		
-		item.setCart(cart);
-		Item i = iRepo.save(item);
-		i = iRepo.findById(i.getId());
-		
-		CartHelper.setCartSize(cart.getItems().size());
-		//return "redirect:/spice?spice=" + item.getSpice().getName(); //change to shop after testing
-		return "redirect:/spices";
-	}
+	*/
+
 }
